b9e2e856925c5bc2791b4d1d4867594e0c5b7fa2
Samfan <samfan@live.com>
2024-07-18 09:53:55 +0530

    feat: Add code to fetch and display a todo from JSON API

---
+++ fetchjson/index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo{
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  //Response.data has properties of id, title, completed
+  const todo = response.data as Todo;
+  const ID = todo.id;
+  const title = todo.title;
+  const finished = todo.completed;
+  
+  console.log(`
+    The todo with ID: ${ID}
+    Has a title of: ${title}
+    Is it finished? ${finished}
+  `);
+});

1a8d363f10a7de6594f145f837666f6cb9ff7b62
Samfan <samfan@live.com>
2024-07-21 12:09:15 +0530

    >>

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -1,1 +1,5 @@
 const carMakers: string[] = ['ford', 'toyota', 'chevy'];
+
+const carsByMake: string[][] = [['f150'], ['corlla'], ['thang']];
+
+const importantDates: (Date | string)[] = [new Date(), '20-10-10'];

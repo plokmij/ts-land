54ae37194c0fed42737151a04ab79fa5ab416eb1
Samfan <samfan@live.com>
2024-07-21 10:06:08 +0530

    annotaion ex1

--- annotations.ts
+++ annotations.ts
@@ -8,3 +8,10 @@ const addNumbers: (a: number, b: number) => number = (a: number, b: number) => {
 }
 
 logNumber(addNumbers(10,15));
+
+
+
+
+const json = '{"x":10,"y":20}';
+const coords: {x: number; y: number} = JSON.parse(json);
+console.log(coords.y);

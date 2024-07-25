39adeef727f705fbb2c5d66897b7c23f632c3180
Samfan <samfan@live.com>
2024-07-25 17:27:42 +0530

    chore: Add CustomMap class for Google Maps integration

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,5 +1,6 @@
 // import {User} from './User';
 // import {Company} from './Company';
+import { CustomMap } from './CustomMap';
 
 // const user = new User();
 // const company = new Company();
@@ -7,10 +8,4 @@
 // console.log(user);
 // console.log(company);
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+new CustomMap('map');

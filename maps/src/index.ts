a8fa015b1443abd71521a57ce5a25c159789fdc9
Samfan <samfan@live.com>
2024-07-25 19:10:16 +0530

    chore: Refactor CustomMap class to use a generic Mappable interface

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,6 +1,7 @@
 import { User } from './User';
 // import {Company} from './Company';
 import { CustomMap } from './CustomMap';
+import { Company } from './Company';
 
 // const user = new User();
 // const company = new Company();
@@ -9,9 +10,11 @@ import { CustomMap } from './CustomMap';
 // console.log(company);
 
 const user = new User();
+const company = new Company();
 const customMap = new CustomMap('map');
 
 
 setTimeout(() => {
-  customMap.addUserMarker(user);
+  customMap.addMarker(user);
+  customMap.addMarker(company);  
 }, 1000);

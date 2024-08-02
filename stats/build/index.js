022010b5bba5d51c698ca5ae5a68b5716a4a5aa3
Samfan <samfan@live.com>
2024-08-03 03:12:51 +0530

    refactor: 1

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log('hi there');
+Object.defineProperty(exports, "__esModule", { value: true });
+const MatchResult_1 = require("./MatchResult");
+const MatchReader_1 = require("./MatchReader");
+const reader = new MatchReader_1.MatchReader('football.csv');
+reader.read();
+const matches = reader.data;
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man united wins ${manUnitedWins} games`);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hi there');
+import { read } from "fs";
+import { CsvFileReader } from "./CsvFileReader";
+import { dateStringToDate } from "./utils";
+import {MatchResult} from './MatchResult';
+import { MatchReader } from "./MatchReader";
+
+const reader = new MatchReader('football.csv');
+reader.read();
+const matches = reader.data;
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man united wins ${manUnitedWins} games`);
